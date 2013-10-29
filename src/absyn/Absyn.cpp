1801160611f95d8258084a56f3b80601d69f4b2f
lileicc <lileicc@gmail.com>
2013-10-28 21:59:40 -0700

    add abstract class of abstract syntax tree root node

---
+++ src/absyn/Absyn.cpp
@@ -0,0 +1,21 @@
+/*
+ * Absyn.cpp
+ *
+ *  Created on: Oct 28, 2013
+ *      Author: leili
+ */
+
+#include "Absyn.h"
+
+namespace swift {
+
+Absyn::Absyn() {
+  // TODO Auto-generated constructor stub
+
+}
+
+Absyn::~Absyn() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace swift */
---
+++ src/absyn/Absyn.h
@@ -0,0 +1,21 @@
+/*
+ * Absyn.h
+ *
+ *  Created on: Oct 28, 2013
+ *      Author: leili
+ */
+
+#ifndef ABSYN_H_
+#define ABSYN_H_
+
+namespace swift {
+
+class Absyn {
+public:
+  Absyn();
+  virtual ~Absyn();
+};
+
+} /* namespace swift */
+
+#endif /* ABSYN_H_ */
