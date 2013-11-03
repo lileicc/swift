cc65cfb50f1f8ca60ed09d9dd52f07187726e5f8
lileicc <lileicc@gmail.com>
2013-11-02 22:43:10 -0700

    add IfThen.h

---
+++ src/ir/IfThen.h
@@ -0,0 +1,20 @@
+/*
+ * IfThen.h
+ *
+ *  Created on: Nov 2, 2013
+ *      Author: leili
+ */
+#pragma once
+
+namespace swift{
+namespace ir{
+
+class IfThen: public Clause {
+public:
+  IfThen();
+  ~IfThen();
+private:
+};
+
+}
+}
