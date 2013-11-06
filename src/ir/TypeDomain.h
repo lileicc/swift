dc85ca98009b06928516848e4166e82e05adc07c
jxwuyi <jxwuyi@gmail.com>
2013-11-05 17:20:39 -0800

    update TypeDomain

--- src/ir/TypeDomain.h
+++ src/ir/TypeDomain.h
@@ -19,19 +19,22 @@ public:
   TypeDomain(const std::string& name = std::string());
   ~TypeDomain();
 
-  const std::string& getName();
+  const std::string& getName() const;
   void addRefer(NameTy* ref);
   NameTy* getRefer();
   void setPreLen(int l);
   int getPreLen();
   void addNumberStmt(NumberStmt* num);
-  const std::vector<NumberStmt*>& getAllNumberStmt();
+  const std::vector<NumberStmt*>& getAllNumberStmt() const;
   NumberStmt* getNumberStmt(int k);
   size_t getNumberStmtSize();
   void addOrigin(OriginAttr* o);
-  const std::vector<OriginAttr*>& getAllOrigin();
+  const std::vector<OriginAttr*>& getAllOrigin() const;
   OriginAttr* getOrigin(int k);
   size_t getOriginSize();
+  void setInstName(int k, std::string name);
+  const std::string& getInstName(int k);
+  const std::vector<std::string>& getInstNames() const;
 
   int getOriginID(const std::string& str);
   OriginAttr* getOrigin(const std::string& str);
@@ -41,6 +44,7 @@ private:
   int prelen;
   std::vector<OriginAttr*> origin;
   NameTy* refer;
+  std::vector<std::string> instName;
 
   std::map<std::string,int> originID;
 };
