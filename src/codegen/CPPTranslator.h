e91f9b212de31eb4f65d8c1886bdcb73aaeec60a
lileicc <lileicc@gmail.com>
2013-11-23 14:41:36 -0800

    adding cpp translator

---
+++ src/codegen/CPPTranslator.h
@@ -0,0 +1,21 @@
+/*
+ * CPPTranslator.h
+ *
+ *  Created on: Nov 23, 2013
+ *      Author: leili
+ */
+
+#pragma once
+
+namespace swift {
+namespace codegen {
+
+class CPPTranslator {
+public:
+  CPPTranslator();
+  virtual ~CPPTranslator();
+};
+
+} /* namespace codegen */
+} /* namespace swift */
+
