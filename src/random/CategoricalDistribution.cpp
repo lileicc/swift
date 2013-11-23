80d6ad8e06afb62eecafb916250793c321e526d0
lileicc <lileicc@gmail.com>
2013-11-23 00:32:38 -0800

    adding categorical distribution

---
+++ src/random/CategoricalDistribution.cpp
@@ -0,0 +1,23 @@
+/*
+ * CategoricalDistribution.cpp
+ *
+ *  Created on: Nov 23, 2013
+ *      Author: leili
+ */
+
+#include "CategoricalDistribution.h"
+
+namespace swift {
+namespace random {
+
+CategoricalDistribution::CategoricalDistribution() {
+  // TODO Auto-generated constructor stub
+
+}
+
+CategoricalDistribution::~CategoricalDistribution() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace random */
+} /* namespace swift */
---
+++ src/random/SwiftDistribution.cpp
@@ -0,0 +1,23 @@
+/*
+ * SwiftDistribution.cpp
+ *
+ *  Created on: Nov 22, 2013
+ *      Author: leili
+ */
+
+#include "SwiftDistribution.h"
+
+namespace swift {
+namespace random {
+
+SwiftDistribution::SwiftDistribution() {
+  // TODO Auto-generated constructor stub
+
+}
+
+SwiftDistribution::~SwiftDistribution() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace random */
+} /* namespace swift */
