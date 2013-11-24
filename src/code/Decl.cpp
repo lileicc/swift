b17263e4c93e2357d021d4516acbfc7ac81ab7b7
lileicc <lileicc@gmail.com>
2013-11-23 17:15:56 -0800

    add syntax tree for target code

---
+++ src/code/Decl.cpp
@@ -0,0 +1,23 @@
+/*
+ * Decl.cpp
+ *
+ *  Created on: Nov 23, 2013
+ *      Author: leili
+ */
+
+#include "Decl.h"
+
+namespace swift {
+namespace code {
+
+Decl::Decl() {
+  // TODO Auto-generated constructor stub
+
+}
+
+Decl::~Decl() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace code */
+} /* namespace swift */
