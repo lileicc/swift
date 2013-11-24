62f03370395aaae22737905cb524ecb27fdf7f68
lileicc <lileicc@gmail.com>
2013-11-23 17:57:32 -0800

    add Code class for target code

---
+++ src/code/Code.h
@@ -0,0 +1,20 @@
+/*
+ * Code.h
+ *
+ *  Created on: Nov 23, 2013
+ *      Author: leili
+ */
+
+#pragma once
+
+namespace swift {
+namespace code {
+
+class Code {
+public:
+  Code();
+  ~Code();
+};
+
+} /* namespace code */
+} /* namespace swift */
