709e994dffa3d9834515803c0e5e4231e179076d
lileicc <lileicc@gmail.com>
2013-11-23 17:57:32 -0800

    add Code class for target code

---
+++ src/code/Code.cpp
@@ -0,0 +1,23 @@
+/*
+ * Code.cpp
+ *
+ *  Created on: Nov 23, 2013
+ *      Author: leili
+ */
+
+#include "Code.h"
+
+namespace swift {
+namespace code {
+
+Code::Code() {
+  // TODO Auto-generated constructor stub
+
+}
+
+Code::~Code() {
+  // TODO Auto-generated destructor stub
+}
+
+} /* namespace code */
+} /* namespace swift */
