5d1a08560c6009df54cd1ab89b5a649e99559636
lileicc <lileicc@gmail.com>
2013-11-23 22:19:20 -0800

    translating typedomain to c++ class, not finished yet

--- src/codegen/CPPTranslator.cpp
+++ src/codegen/CPPTranslator.cpp
@@ -7,8 +7,18 @@
 
 #include "CPPTranslator.h"
 
+
 namespace swift {
 namespace codegen {
+
+namespace {
+std::string getVarOfNumType(std::string name) {
+  return "__num_" + name;
+}
+}
+
+static std::shared_ptr<code::QualType> INT_TYPE(new code::QualType("int"));
+static std::shared_ptr<code::QualType> DOUBLE_TYPE(new code::QualType("double"));
 
 CPPTranslator::CPPTranslator() {
   // TODO Auto-generated constructor stub
@@ -24,11 +34,26 @@ void CPPTranslator::translate(swift::ir::BlogModel* model) {
     transTypeDomain(ty);
 }
 
-code::Code* CPPTranslator::getResult() const {
+std::shared_ptr<code::Code> CPPTranslator::getResult() const {
+  return prog;
 }
 
 void CPPTranslator::transTypeDomain(std::shared_ptr<ir::TypeDomain> td) {
+  const std::string& name = td->getName();
+  std::shared_ptr<code::ClassDecl> cd = code::ClassDecl::createClassDecl(coreNs, name);
+  int len = td->getPreLen();
+  if (len > 0) {
+    std::string numvar = getVarOfNumType(name);
+    code::FieldDecl::createFieldDecl(coreCls, numvar, INT_TYPE);
+
+  }
 }
+
+
 
 } /* namespace codegen */
 } /* namespace swift */
+
+
+
+
