85c9b64c353ecf210734e22a0427b31ba4ab8dcc
tejasvsrinivasan <tejasvsrinivasan@gmail.com>
2014-01-08 23:13:12 -0500

    removed compile.bat and test.bat

--- src/compile.bat
+++
@@ -1,5 +0,0 @@
-cd parse
-yacc -d blog.yacc
-flex blog.flex
-cd ..
-g++ -std=gnu++11 absyn\*.cpp ir\*.cpp fabrica\*.cpp msg\*.cpp predecl\*.cpp semant\Semant.cpp main.cpp parse\y.tab.c parse\lex.yy.c -o parseex
--- src/test.bat
+++
@@ -1,4 +0,0 @@
-for %%f in (test\parse\*.in) do (
-  parseex -i test\parse\%%~nf.in -o test\parse\%%~nf.tmp
-  fc test\parse\%%~nf.out test\parse\%%~nf.tmp
-)
