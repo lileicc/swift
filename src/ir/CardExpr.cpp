a3868a3fb86e4df3d712eb6520841e0e875668ec
lileicc <lileicc@gmail.com>
2014-01-28 02:40:46 -0800

    CardExpr refer —> referTyp

--- src/ir/CardExpr.cpp
+++ src/ir/CardExpr.cpp
@@ -24,7 +24,7 @@ const std::shared_ptr<SetExpr>& CardExpr::getBody() const {
 
 void CardExpr::print(FILE* file, int indent){
   fprintf(file, "%*s(CardExpr:\n", indent, "");
-  fprintf(file, "%*srefer: %s\n", indent+2, "", refer->toString().c_str());
+  fprintf(file, "%*sreferTyp: %s\n", indent+2, "", referTyp->toString().c_str());
   fprintf(file, "%*sbody:\n", indent+2, "");
   getBody()->print(file, indent+4);
   fprintf(file, "%*s)\n", indent, "");
