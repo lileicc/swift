ffa3a3e4384e38e9f3bdb279d8741a895292f7b0
jxwuyi <jxwuyi@gmail.com>
2014-02-08 23:00:11 +0800

    add Dirichlet Distribution

---
+++ src/random/Dirichlet.h
@@ -0,0 +1,31 @@
+/*
+ * Beta.h
+ *
+ *  Created on: Feb 6, 2014
+ *      Author: yiwu
+ */
+
+#pragma once
+#include <random>
+#include <vector>
+#include "SwiftDistribution.h"
+
+namespace swift {
+namespace random {
+
+class Dirichlet: public swift::random::SwiftDistribution<std::vector<double> > {
+public:
+  Dirichlet();
+  virtual ~Dirichlet();
+  void init(std::vector<double> alpha);
+  std::vector<double> gen();
+  double likeli(std::vector<double> x);
+  double loglikeli(std::vector<double> x);
+private:
+  std::vector<std::gamma_distribution<double> > dist_alpha;
+  std::vector<double> alpha;
+  double coef, log_coef;
+};
+
+} /* namespace random */
+} /* namespace swift */
