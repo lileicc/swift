a81601ee65d32f9eb9f6a8854d88a20cded7b5bd
lileicc <lileicc@gmail.com>
2014-02-07 22:57:35 -0800

    fix compile.bat

--- compile.bat
+++ compile.bat
@@ -1,7 +1,1 @@
-cd src
-cd parse
-yacc -d -o parser.cpp blog.yacc
-flex -olexer.cpp blog.flex
-cd ..
-cd ..
-g++ -std=gnu++11 src/absyn/*.cpp src/ir/*.cpp src/fabrica/*.cpp src/msg/*.cpp src/predecl/*.cpp src/code/*.cpp src/codegen/CPPTranslator.cpp src/semant/Semant.cpp src/main.cpp src/parse/parser.cpp src/parse/lexer.cpp -o parseex
+g++ -std=gnu++11 src/absyn/*.cpp src/ir/*.cpp src/fabrica/*.cpp src/msg/*.cpp src/predecl/*.cpp src/code/*.cpp src/codegen/CPPTranslator.cpp src/semant/Semant.cpp src/main.cpp src/parse/parser.cpp src/parse/lexer.cpp -o swift
