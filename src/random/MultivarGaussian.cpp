b01041b9600afae885ddd7f6001674914417cf03
jxwuyi <jxwuyi@gmail.com>
2014-09-23 22:38:34 -0700

    add namespace to MultivarGaussian.cpp

--- src/random/MultivarGaussian.cpp
+++ src/random/MultivarGaussian.cpp
@@ -23,17 +23,17 @@ void MultivarGaussian::init(arma::mat _mean, arma::mat _cov) {
   cov = _cov;
   // Dimension Check
   if (mean.n_rows == 1)
-    mean = trans(mean);
+    mean = arma::trans(mean);
   assert(mean.n_cols == 1);
   assert(mean.n_rows == cov.n_rows && cov.n_cols == cov.n_rows);
   c.set_size(mean.n_rows, mean.n_cols);
-  A = trans(chol(cov));
+  A = arma::trans(arma::chol(cov));
   dist = std::normal_distribution<double>(0, 1);
 
   // Variables for computing cpd
   k = mean.n_rows;
-  inv_cov = inv_sympd(cov); // Assume it is positive definite!
-  det_cov = det(cov);
+  inv_cov = arma::inv_sympd(cov); // Assume it is positive definite!
+  det_cov = arma::det(cov);
   coef = std::pow(std::sqrt(PI * 2.0), -k) / std::sqrt(det_cov);
   log_coef = -k * 0.5 * std::log(2.0 * PI) - 0.5 * std::log(det_cov);
 }
@@ -45,13 +45,13 @@ arma::mat MultivarGaussian::gen() {
 
 double MultivarGaussian::likeli(const arma::mat& x) {
   arma::mat v(x - mean);
-  double rat = as_scalar(trans(v) * inv_cov * v);
+  double rat = arma::as_scalar(arma::trans(v) * inv_cov * v);
   return coef * std::exp(-0.5 * rat);
 }
 
 double MultivarGaussian::loglikeli(const arma::mat& x) {
   arma::mat v(x - mean);
-  double rat = as_scalar(trans(v) * inv_cov * v);
+  double rat = arma::as_scalar(arma::trans(v) * inv_cov * v);
   return log_coef - 0.5 * rat;
 }
 
