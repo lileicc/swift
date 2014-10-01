31e0e79e3e795dda9b6303105f5550d142b230f7
jxwuyi <jxwuyi@gmail.com>
2014-10-01 11:10:14 -0700

    update Beta

--- src/random/Beta.cpp
+++ src/random/Beta.cpp
@@ -12,7 +12,8 @@ namespace swift {
 namespace random {
 
 Beta::Beta() :
-    alpha(2.0), beta(2.0) {
+    alpha(2.0), beta(2.0),
+    is_dist_ok(false), is_like_ok(false), is_loglike_ok(false) {
 }
 
 Beta::~Beta() {
@@ -21,23 +22,36 @@ Beta::~Beta() {
 void Beta::init(double alpha, double beta) {
   this->alpha = alpha;
   this->beta = beta;
-  coef = std::tgammal((long double)(alpha + beta)) *
-  (1.0L / std::tgammal((long double)alpha)) * (1.0L / std::tgammal((long double)beta));
-  log_coef = std::lgammal((long double)(alpha+beta)) - std::lgammal((long double)alpha) - std::lgammal((long double)beta);
-  dist_alpha = std::gamma_distribution<double>(alpha);
-  dist_beta = std::gamma_distribution<double>(beta);
+  is_dist_ok = false;
+  is_like_ok = false;
+  is_loglike_ok = false;
 }
 
 double Beta::gen() {
+  if(!is_dist_ok) {
+    dist_alpha = std::gamma_distribution<double>(alpha);
+    dist_beta = std::gamma_distribution<double>(beta);    
+    is_dist_ok = true;
+  }
   double x = dist_alpha(engine);
   return x / (x + dist_beta(engine));
 }
 
 double Beta::likeli(const double& x) {
+  if(!is_like_ok){
+    coef = std::tgammal((long double)(alpha + beta)) *
+      (1.0L / std::tgammal((long double)alpha)) * (1.0L / std::tgammal((long double)beta));
+    is_like_ok = true;
+  }
   return coef * pow(x, alpha - 1.0) * pow(1.0 - x, beta - 1.0);
 }
 
 double Beta::loglikeli(const double& x) {
+  if(!is_loglike_ok){
+    log_coef = std::lgammal((long double)(alpha+beta)) 
+      - std::lgammal((long double)alpha) - std::lgammal((long double)beta);
+    is_loglike_ok = true;
+  }
   return log_coef + (alpha - 1.0) * x + (beta - 1.0) * (1.0 - x);
 }
 
