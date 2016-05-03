395d363bdfb8c0482d3fe3b2652e3d358db2a76c
jxwuyi <jxwuyi@gmail.com>
2016-05-03 00:51:00 -0700

    add comments and fix typo

--- src/util/util_matrix.h
+++ src/util/util_matrix.h
@@ -34,9 +34,11 @@ namespace swift {
 #define _predecl_colsum(a) (arma::sum(a,0))
 #define _predecl_rowsum(a) (arma::sum(a,1))
 
+// now a hack to fix the compilation issue
+// in armadillo, a==b yields a matrix instead of a bool
 static const double MAT_EPS = 1e-10;
 bool operator == (const mat&a, const mat&b) { return arma::approx_equal(a, b, "absdiff", MAT_EPS); }
-bool operator != (const mat&a, const mat&b) { return arma::approx_equal(a, b, "absdiff", MAT_EPS); }
+bool operator != (const mat&a, const mat&b) { return !arma::approx_equal(a, b, "absdiff", MAT_EPS); }
 
 
 // Special Case for toInt
@@ -189,7 +191,7 @@ mat loadRealMatrix(std::string filename, int x1 = -1, int x2 = -1, int y1 = -1, 
 //     MCMC proposals
 ////////////////////////////////////////
 // Util for Uni-Gaussian Proposal
-mat _mutligaussian_prop(const mat& x) {
+mat _multigaussian_prop(const mat& x) {
   static double var = 0.01;
   static std::normal_distribution<double> ndist(0, var);
   mat ret = x;
