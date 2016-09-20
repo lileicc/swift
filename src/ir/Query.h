625863bdc75a1c01a483771185d39ef515921944
Giulio Zhou <giuliozhou8@gmail.com>
2016-09-20 15:47:00 -0700

    Add support for for-loop query in Semant

--- src/ir/Query.h
+++ src/ir/Query.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 #include "FunctionCall.h"
 
 namespace swift {
@@ -9,12 +10,26 @@ namespace ir {
 class Query {
 public:
   Query(std::shared_ptr<Expr> var, std::string stringExpr = "", bool general = false);
+  Query(std::shared_ptr<Expr> var, std::vector<std::shared_ptr<VarDecl> > args,
+        std::string stringExpr = "", bool general = false,
+        std::shared_ptr<Expr> cond = nullptr);
   virtual ~Query();
 
   // TODO refactor name
   const std::shared_ptr<Expr>& getVar() const;
   // For Debugging Use
   void print(FILE* file, int indent);
+
+  // for loop quantifying
+  bool hasForLoop() const;
+  const std::shared_ptr<Expr>& getCond() const;
+  const std::shared_ptr<VarDecl> getArg(int k) const;
+  const std::vector<std::shared_ptr<VarDecl> >& getArgs() const;
+  const int argSize() const;
+  // Temporal features
+  bool isTemporal() const;
+  std::shared_ptr<VarDecl> getTemporalArg();
+  void processTemporal(const Ty* timety);
 
   // whether var is special or general
   bool isGeneral() const;
@@ -29,6 +44,16 @@ private:
   std::shared_ptr<Expr> var;
   std::string strExpr;
   bool general;
+
+  // whether this obs stmt has a for-loop
+  bool flag_forloop; 
+  // var decls in the for loop
+  std::vector<std::shared_ptr<VarDecl> > args;
+  // temporal argument in the for loop
+  bool istmp;
+  std::shared_ptr<VarDecl> tmparg;
+  // condition in the for loop
+  std::shared_ptr<Expr> cond;
 };
 
 }
